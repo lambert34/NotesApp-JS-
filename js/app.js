a53ac86828966a9630f196f5be8f3884963af24d
alexL <aleksandrlambert@gmail.com>
2024-03-26 19:07:50 +0700

    добавили отслеживание события по id
    и добавили обработчик событий при активации 'input'

--- js/app.js
+++ js/app.js
@@ -13,27 +13,42 @@ function createNote(title, text) {
     <p id="note-title">${title}</p>
     <textarea id="note-title-input" class="hidden">${title}</textarea>
     <div>
-    <button class="note-edit"><i class="fa-solid fa-pen-to-square"></i></button>
-    <button class="note-delete"><i class="fa-solid fa-trash"></i></button>
+        <button class="note-edit"><i class="fa-solid fa-pen-to-square"></i></button>
+        <button class="note-delete"><i class="fa-solid fa-trash"></i></button>
     </div>
     </div>
     <p id="note-text">${text}</p>
+    <textarea id="note-textarea" class="hidden">${text}</textarea>
     `;
 
     const editBtn = noteEl.querySelector('.note-edit');
     const deleteBtn = noteEl.querySelector('.note-delete');
 
     const titleEl = noteEl.querySelector('#note-title');
-    const textEl = noteEl.querySelector('#note-title');
+    const textEl = noteEl.querySelector('#note-text');
+    const titleInputEl = noteEl.querySelector('#note-title-input');
+    const textInputEl = noteEl.querySelector('#note-textarea');
+
 
     editBtn.addEventListener('click', (e) => {
         titleEl.classList.toggle('hidden');
         textEl.classList.toggle('hidden');
+
+        titleInputEl.classList.toggle('hidden');
+        textInputEl.classList.toggle('hidden');
     });
 
     deleteBtn.addEventListener('click', (e) => {
         noteEl.remove(); //данный метод удаляет описанный дом-узел 
-    })
+    });
+
+    titleInputEl.addEventListener('input', (e) => {
+        titleEl.innerText = e.target.value;
+    });
+
+    textInputEl.addEventListener('input', (e) => {
+        textEl.innerText = e.target.value;
+    });
 
     return noteEl;
 };
@@ -41,5 +56,4 @@ function createNote(title, text) {
 addBtn.addEventListener('click', (e) => {
     const el = createNote('Заголовок', 'Текст заметки');
     notesEl.appendChild(el);
-})
-
+});
